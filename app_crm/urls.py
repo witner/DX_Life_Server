23c2a6bfd654a6a686c73f72d14555437111492b
黄冬冬 <31064748+witner@users.noreply.github.com>
2019-10-30 22:08:28 +0800

    登录验证码代码优化

--- app_crm/urls.py
+++ app_crm/urls.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 # 第三方模块
 from django.urls import path
-from .views.user_info import UserLogin
+from .views.user_info import get_pic_code
 # 导入自定义模块
 
 # 设置环境变量
 
 urlpatterns = [
     # 登录、注销、注册
-    path('user/login/', UserLogin.as_view(), name='user_login'),
-    # path('user/login/get_pic_code', UserLogin.as_view(), name='user_login'),
+    # path('user/login/', UserLogin.as_view(), name='user_login'),
+    path('user/login/get_pic_code', get_pic_code, name='user_login'),
 ]
--- app_crm/views/user_info.py
+++ app_crm/views/user_info.py
@@ -8,13 +8,16 @@ import os
 # 第三方模块
 from rest_framework.views import APIView
 from PIL import Image, ImageDraw, ImageFont
+from django.conf import settings
+# settings.configure()
+from django.shortcuts import render, HttpResponse, redirect
 
 
 def get_pic_code(request):
     """
     获取验证码
     :param request:
-    :return:
+    :return: 返回图片流
     """
     if request.method == 'GET':
         text_num = 4        # 验证码字符个数
@@ -22,46 +25,10 @@ def get_pic_code(request):
         pic_width = request.GET.get('pic_width', 150)
         pic_height = request.GET.get('pic_height', 26)
 
-
         img = Image.new('RGB', (pic_width, pic_height), color='white')
         draw = ImageDraw.Draw(img)
         # ttf_file_path = os.path.join(settings.BASE_DIR, 'app_c_rbac/static/ttf/经典行书简.TTF'),
         font = ImageFont.truetype(os.path.join(settings.BASE_DIR, 'app_crm/static/ttf/经典行书简.TTF'), size=text_size)
-    pass
-
-
-
-class LoginPicCode(APIView):
-    pass
-
-
-
-class UserLogin(APIView):
-    """
-    用户登录
-    """
-    def get(self, request):
-        """
-        get 请求登录页面
-        :param request:
-        :return:
-        """
-        pass
-
-    def get_pic_code(self, request, pic_width=150, pic_height=26, text_num=4, text_size=20):
-        """
-        登录验证码
-        :param request:
-        :param pic_width: 图片宽度
-        :param pic_height: 图片高度
-        :param text_num: 验证码字符个数
-        :param text_size: 验证码字符大小
-        :return:
-        """
-        img = Image.new('RGB', (pic_width, pic_height), color='white')
-        draw = ImageDraw.Draw(img)
-        # ttf_file_path = os.path.join(settings.BASE_DIR, 'app_c_rbac/static/ttf/经典行书简.TTF'),
-        font = ImageFont.truetype(os.path.join(settings.BASE_DIR, 'app_c_rbac/static/ttf/经典行书简.TTF'), size=text_size)
 
         f = BytesIO()
 
@@ -93,4 +60,18 @@ class UserLogin(APIView):
         request.session['str_pic_code'] = str_pic_code
 
         return HttpResponse(data)
+
+
+class UserLogin(APIView):
+    """
+    用户登录
+    """
+    def get(self, request):
+        """
+        get 请求登录页面
+        :param request:
+        :return:
+        """
+        pass
+
 
