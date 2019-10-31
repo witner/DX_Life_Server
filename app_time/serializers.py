d76bc28e093541fe31437d252edfb875dd913875
huangdongdong <huangdongdong@daojia-inc.com>
2019-10-31 18:23:27 +0800

    app_time 1、event事件添加接口

--- app_time/serializers.py
+++ app_time/serializers.py
@@ -13,7 +13,7 @@ class EventSerializer(serializers.Serializer):
 
 
 class DoneSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(required=False)
     title = serializers.CharField(max_length=128)
     start_date = serializers.DateField()
     start_time = serializers.TimeField()
@@ -21,9 +21,25 @@ class DoneSerializer(serializers.Serializer):
     happiness = serializers.IntegerField()
     health = serializers.IntegerField()
     fulfillment = serializers.IntegerField()
-    event = EventSerializer()
-    user = UserInfoSerializer()
+    event = EventSerializer(read_only=True)
+    # 设置反序列字段
+    event_id = serializers.IntegerField(write_only=True)
+    user = UserInfoSerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
 
     # class Meta:
     #     model = Done
     #     fields = '__all__'
+
+    def create(self, validated_data):
+        done_obj = Done.objects.create(title=validated_data['title'],
+                                       start_date=validated_data['start_date'],
+                                       start_time=validated_data['start_time'],
+                                       duration=validated_data['duration'],
+                                       happiness=validated_data['happiness'],
+                                       health=validated_data['health'],
+                                       fulfillment=validated_data['fulfillment'],
+                                       event_id=validated_data['event_id'],
+                                       user_id=validated_data['user_id'])
+        return done_obj
+
--- app_time/views/done.py
+++ app_time/views/done.py
@@ -16,3 +16,11 @@ class DoneView(APIView):
         ret = DoneSerializer(done_list, many=True)
         return Response(ret.data)
         pass
+
+    def post(self, request):
+        serializer_obj = DoneSerializer(data=request.data)
+        if serializer_obj.is_valid():
+            serializer_obj.save()
+            return Response(serializer_obj.validated_data)
+        else:
+            return Response(serializer_obj.errors)
