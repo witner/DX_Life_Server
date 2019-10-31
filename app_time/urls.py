46233762b211e408ca229ebf161ec3ed9fdeb157
黄冬冬 <31064748+witner@users.noreply.github.com>
2019-10-31 22:42:38 +0800

    app_time: 完善event的请求接口

--- app_time/urls.py
+++ app_time/urls.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 from app_time.views.done import DoneView
+from app_time.views.event import EventView
 
 urlpatterns = [
     path('done/', DoneView.as_view(), name='done'),
+    path('event/', EventView.as_view()),
 ]
---
+++ app_time/views/event.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 导入系统模块
+
+# 第三方模块
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# 导入自定义模块
+from app_time.serializers import EventSerializer
+from app_time.models import Event
+# 设置环境变量
+
+
+class EventView(APIView):
+    def get(self, request):
+        """
+        请求获取事件信息，
+        :param request: 接受请求参数【parent_id、level】
+        :return:
+        """
+        response_dict = {
+            'code': 200,
+            'data': None,
+        }
+
+        # 获取查询条件
+        parent_id = request.query_params.get('parent_id', 0)
+        level = request.query_params.get('level', 0)
+        try:
+            parent_id = int(parent_id)
+            level = int(level)
+        except Exception:
+            response_dict.update({
+                'code': 400,
+                'data': '请求参数错误，不能为非数字',
+            })
+            return Response(response_dict)
+        else:
+            # 数据库查询
+            event_list = Event.objects.all()
+            if parent_id != 0:
+                event_list = event_list.filter(parent_id=parent_id)
+
+            if level != 0:
+                event_list = event_list.filter(level=level)
+
+            ret = EventSerializer(event_list, many=True)
+            response_dict.update({
+                'data': ret.data
+            })
+            return Response(response_dict)
+
+
+
+
